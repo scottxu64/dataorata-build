f81051a5e69e2ad7cc17a6ef78cb7c039d672312
Scott <scottxu64@gmail.com>
2019-10-19 10:51:08 -0400

    init public build

---
+++ precache-manifest.60b53f1227429fc8102286edcf5bb6f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aa84bfd52fe950b2351082f64d8a70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edda1ef45e9eecb3c153",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c20ed9336fb08c541fb4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "edda1ef45e9eecb3c153",
+    "url": "/static/js/2.1cf094d7.chunk.js"
+  },
+  {
+    "revision": "c20ed9336fb08c541fb4",
+    "url": "/static/js/main.9a2fe019.chunk.js"
+  },
+  {
+    "revision": "d97967b42865efdd0517",
+    "url": "/static/js/runtime-main.12238a90.js"
+  }
+]);
